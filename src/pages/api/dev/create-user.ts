3859a85a8ae9c13890f0459e70ed542a554623f2
Kiseki 奇跡 <contact@kiseki-miracle.dev>
2026-07-10 00:06:26 -0500

    removed old turso calls from api endpoints

--- src/pages/api/dev/create-user.ts
+++ src/pages/api/dev/create-user.ts
@@ -1,15 +1,14 @@
 import type { APIRoute } from "astro";
-import { execute, toObjects } from "@/lib/db";
 
 export const GET: APIRoute = async () => {
   try {
-    await execute(
-      /* sql */ `
-      INSERT INTO users (id, username, email)
-        VALUES  (?, ?, ?);
-    `,
-      [crypto.randomUUID(), "KisekiMiracle", "kiseki.miracle247@gmail.com"],
-    );
+    // await execute(
+    //   /* sql */ `
+    //   INSERT INTO users (id, username, email)
+    //     VALUES  (?, ?, ?);
+    // `,
+    //   [crypto.randomUUID(), "KisekiMiracle", "kiseki.miracle247@gmail.com"],
+    // );
 
     return Response.json(
       {
--- src/pages/api/dev/test.ts
+++ src/pages/api/dev/test.ts
@@ -1,12 +1,6 @@
 import type { APIRoute } from "astro";
-import { execute, toObjects } from "@/lib/db";
 
 export const GET: APIRoute = async () => {
-  const data = await execute(/* sql */ `
-    SELECT * FROM comments
-  `);
-
-  console.log("data", toObjects(data));
   return Response.json({
     success: true,
   });
